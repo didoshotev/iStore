d213bca91a6376f2e71d4a2ed4aea0e4cba0f5d5
Didoshotev <didoshotev@gmail.com>
2021-03-18 17:21:20 +0200

    feat/ready made backend added

---
+++ server/controllers/products.js
@@ -0,0 +1,59 @@
+const models = require('../models');
+
+module.exports = {
+    get: (req, res, next) => {
+        // console.log(req.query);
+        // const length = req.query.length ? parseInt(req.query.length) : 20
+        models.Product.find()
+            .then((products) => res.send(products))
+            .catch(next);
+    },
+
+    getIphones: (req, res, next) => {
+        const iphone = req.params.iphone
+        models.Product.find({ deviceType: iphone})
+        .then((products) => res.send(products))
+        .catch(next);
+    },
+    getMacs: (req, res, next) => {
+        const mac = req.params.mac
+        models.Product.find({ deviceType: mac})
+        .then((products) => res.send(products))
+        .catch(next);
+    },
+    getIpads: (req, res, next) => {
+        const ipad = req.params.ipad
+        models.Product.find({ deviceType: ipad})
+        .then((products) => res.send(products))
+        .catch(next);
+    },
+    getAccessories: (req, res, next) => {
+        const accessory = req.params.accessory
+        models.Product.find({ deviceType: accessory})
+        .then((products) => res.send(products))
+        .catch(next);
+    },
+
+    post: (req, res, next) => {
+        const { title, description, imageUrl, price, deviceType, isActive } = req.body;
+        models.Product.create({ title, description, imageUrl, price, deviceType, isActive })
+            .then((createdProduct) => {
+                res.send(createdProduct);
+            }).catch(next);
+    },
+
+    put: (req, res, next) => {
+        const id = req.params.id;
+        const { title, description, imageUrl, price, deviceType, isActive } = req.body;
+        models.Product.updateOne({ _id: id }, { title, description, imageUrl, price, deviceType, isActive })
+            .then((updatedProduct) => res.send(updatedProduct))
+            .catch(next)
+    },
+
+    delete: (req, res, next) => {
+        const id = req.params.id;
+        models.Product.deleteOne({ _id: id })
+            .then((removedProduct) => res.send(removedProduct))
+            .catch(next)
+    }
+};
---
+++ server/routes/products.js
@@ -0,0 +1,21 @@
+const controllers = require('../controllers/');
+const router = require('express').Router();
+const { auth } = require('../utils');
+
+router.get('/', controllers.products.get);
+
+router.get('/:iphone', controllers.products.getIphones);
+
+router.get('/:ipad', controllers.products.getIpads);
+
+router.get('/:mac', controllers.products.getMacs);
+
+router.get('/:accessory', controllers.products.getAccessories);
+
+router.post('/', auth(), controllers.products.post);
+
+router.put('/:id', auth(), controllers.products.put);
+
+router.delete('/:id', auth(), controllers.products.delete);
+
+module.exports = router;
