704ad9cfb4d50c50a5e726263c2e0abb01fb2ba2
Didoshotev <didoshotev@gmail.com>
2021-03-26 16:22:30 +0200

    fix/add files from prev branch

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import UserContext from './Context'
+
+function getCookie(name) {
+    const cookieValue = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return cookieValue ? cookieValue[2] : null;
+}
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            loggedIn: null,
+            user: null
+        }
+    }
+
+    logIn = (user) => {
+        this.setState({
+            loggedIn: true,
+            user
+        })
+    }
+
+    logOut = () => {
+        document.cookie = 'x-auth-token= ; expires= Thu, 01 Jan 1970 00:00:00 GMT'
+        this.setState({
+            loggedIn: false,
+            user: null
+        })
+    }
+
+    componentDidMount() {
+        const token = getCookie('x-auth-token')
+        if(!token) {
+            this.logOut()
+            return
+        }
+
+        fetch('http://localhost:5000/api/user/verify', {
+            method: 'POST',
+            body: JSON.stringify({
+                token
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(promise => {
+            return promise.json()
+        }).then(response => {
+            if(response.status) {
+                this.logIn({
+                    username: response.user.username,
+                    id: response.user._id,
+                    role: response.user.role
+                })
+            } else {
+                this.logOut()
+            }
+        })
+    }
+
+    render() {
+        const {
+            loggedIn,
+            user
+        } = this.state
+        if(loggedIn === null) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        return (
+            <UserContext.Provider value={{
+                loggedIn,
+                user,
+                logIn: this.logIn,
+                logOut: this.logOut
+            }}>
+               
+                {this.props.children}
+               
+            </UserContext.Provider>
+            
+        )
+    }
+}
+
+export default App
---
+++ src/utils/authenticate.js
@@ -0,0 +1,32 @@
+const authenticate = async (url, body, onSuccess, onFailure) => {
+
+    try {
+        const promise = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if(promise.status === 500) {
+            onFailure(promise.statusText)
+            return
+        }
+        const authToken = promise.headers.get('Authorization')
+        document.cookie = `x-auth-token=${authToken}`
+        const response = await promise.json()
+        if (response.username && authToken) {
+            onSuccess({
+                username: response.username,
+                id: response._id
+            })
+        } else {
+            onFailure()
+        }
+    } catch (err) {
+        onFailure()
+    }
+}
+
+export default authenticate
