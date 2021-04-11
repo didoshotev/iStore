59e90a2f2ff65bad008c0a495c985c889d933fd8
Didoshotev <didoshotev@gmail.com>
2021-04-11 14:07:49 +0300

    fix/routes

---
+++ src/pages/not-found-page/not-found-page.js
@@ -0,0 +1,15 @@
+import PageLayout from "../../components/global/page-layout/page-layout"
+
+const NotFoundPage = () => {
+
+    return (
+        <PageLayout>
+            <div>
+                <h2>Page not found...</h2>
+            </div>
+        </PageLayout>
+
+    )
+}
+
+export default NotFoundPage
